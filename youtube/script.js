7aea678788df9f3baa4c153184eba53da026543b
Qwe1234321as <135865553+Qwe1234321as@users.noreply.github.com>
2023-06-11 16:03:07 +0530

    Delete youtube directory

--- youtube/script.js
+++
@@ -1,9 +0,0 @@
-var menuIcon =document.querySelector(".menu");
-var itemsdownleft =document.querySelector(".items-down-left");
-var container=document.querySelector(".container");
-
-menuIcon.onclick= function(){
-      itemsdownleft.classList.toggle("small-itemsdownlist");
-      container.classList.toggle("large-container");
-}
-
